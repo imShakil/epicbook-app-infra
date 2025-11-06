23824497037ae095a90130368bc10e4e6de03b4d
Global Mobarak <mh.ice.iu@gmail.com>
2025-11-06 17:23:44 +0600

    fixed pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,18 +2,13 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu_ami" {
+data "aws_ami" "os_image" {
     most_recent = true
-    owners = ["099720109477"]
+    owners = ["amazon"]
 
     filter {
       name = "name"
-      values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-    }
-
-    filter {
-      name = "virtualization-type"
-      values = ["hvm"]
+      values = ["al2023-ami-*-x86_64"]
     }
 }
 
@@ -26,7 +21,7 @@ module "network" {
 module "vm" {
   source = "git::https://github.com/imShakil/tfmodules.git//aws/instance"
 
-  ami_id = data.aws_ami.ubuntu_ami.id
+  ami_id = data.aws_ami.os_image.id
   subnet_id = module.network.vpc_attribute.public_subnet_ids[0]
   vpc_security_group_ids = [module.network.vpc_attribute.security_group_id]
   ssh_key_pair = {
